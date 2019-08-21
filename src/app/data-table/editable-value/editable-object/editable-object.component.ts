619ca62f1800668e8ef26e9b7651e2241d5a13c3
daniel <griebdr@gmail.com>
2019-08-21 18:13:55 +0300

    rewriting talbeinfo

---
+++ src/app/data-table/editable-value/editable-object/editable-object.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import EditableValue from '../editable-value';
+import { TableInfo } from '../editable-type';
+import { Observable, Subject } from 'rxjs';
+import { MatDialog, MatDialogRef } from '@angular/material';
+
+import * as Lodash from 'lodash';
+import { EditableOpenObjectComponent } from './editable-open-object/editable-open-object.component';
+
+@Component({
+  selector: 'app-editable-object',
+  templateUrl: './editable-object.component.html',
+  styleUrls: ['./editable-object.component.scss']
+})
+export class EditableObjectComponent implements OnInit {
+  @Input() typeInfo: TableInfo;
+  @Input() value: object;
+  @Output() save = new EventEmitter<object[]>();
+  @Output() cancel = new EventEmitter<void>();
+
+  _open: boolean;
+  dialogRef: MatDialogRef<any>;
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit() {
+
+  }
+
+  set open(open: boolean) {
+    this._open = open;
+
+    if (open === true) {
+      this.dialogRef = this.dialog.open(EditableOpenObjectComponent, {
+        width: '320px',
+        data: { value: this.value, typeInfo: this.typeInfo, title: 'Edit' },
+        autoFocus: false,
+        disableClose: true
+      });
+
+      this.dialogRef.afterClosed().subscribe(result => {
+        this._open = false;
+        if (result !== undefined) {
+          this.save.emit(Lodash.cloneDeep(result));
+        }
+      });
+    } else {
+      this.dialogRef.close();
+    }
+  }
+
+  get open() {
+    return this._open;
+  }
+
+  get showedValue() {
+    let value = '';
+    for (const key in this.value) {
+      if (this.value.hasOwnProperty(key)) {
+        const element = this.value[key];
+        value += ' ' + element;
+      }
+    }
+
+    return value.substr(0, 12) + ' ...';
+  }
+}
---
+++ src/app/data-table/editable-value/editable-object/editable-open-object/editable-open-object.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Inject, Input, ViewChildren, QueryList } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { TableInfo } from '../../editable-type';
+import EditableValue from '../../editable-value';
+import * as Lodash from 'lodash';
+
+@Component({
+  selector: 'app-editable-open-object',
+  templateUrl: './editable-open-object.component.html',
+  styleUrls: ['./editable-open-object.component.scss']
+})
+export class EditableOpenObjectComponent implements OnInit {
+  value = {};
+
+  @ViewChildren('editableValue') editableValues: QueryList<any>;
+
+  constructor(
+    public dialogRef: MatDialogRef<EditableOpenObjectComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { value: object, typeInfo: TableInfo, title: string }
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    if (this.data.value === undefined) {
+      this.value = {};
+    } else {
+      this.value = Lodash.cloneDeep(this.data.value);
+    }
+  }
+
+  get openedEditableValue() {
+    let editableValue2: any;
+
+    this.editableValues.forEach(editableValue => {
+      if (editableValue.open === true) {
+        editableValue2 = editableValue;
+      }
+    });
+
+    return editableValue2;
+  }
+
+  openEditableValue(editableValue: EditableValue) {
+    if (!this.openedEditableValue) {
+      editableValue.open = true;
+    }
+  }
+}
